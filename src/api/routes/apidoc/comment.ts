4e41a6b2739b403e29e9a133e54b917d84c7cea5
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2025-02-17 19:47:39 +0200

    fix: update API documentation to use plural endpoints and improve response structure

--- src/api/routes/apidoc/comment.ts
+++ src/api/routes/apidoc/comment.ts
@@ -1,5 +1,5 @@
 /**
- * @api {get} /comment Get All Comments
+ * @api {get} /comments Get All Comments
  * @apiName GetCommentList
  * @apiGroup Comment
  *
@@ -25,7 +25,7 @@
  * @apiError CommentsNotFound No comments found
  */
 /**
- * @api {post} /comment Post Comment
+ * @api {post} /comments Post Comment
  * @apiName PostComment
  * @apiGroup Comment
  *
@@ -51,28 +51,24 @@
  *
  * @apiParam {Number} id Media's unique ID.
  *
- * @apiSuccess {String} message Success message
- * @apiSuccess {Object[]} comments List of comments for the media
- * @apiSuccess {Number} comments.comment_id ID of the comment
- * @apiSuccess {Number} comments.user_id ID of the user who commented
- * @apiSuccess {Number} comments.media_id ID of the media
- * @apiSuccess {String} comments.comment_text Content of the comment
- * @apiSuccess {String} comments.created_at Timestamp when created
+ * @apiSuccess {Object[]} Array of comments for the media
+ * @apiSuccess {Number} comment_id ID of the comment
+ * @apiSuccess {Number} user_id ID of the user who commented
+ * @apiSuccess {Number} media_id ID of the media
+ * @apiSuccess {String} comment_text Content of the comment
+ * @apiSuccess {String} created_at Timestamp when created
  *
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
- *     {
- *       "message": "Comments found",
- *       "comments": [
- *         {
- *           "comment_id": 1,
- *           "user_id": 1,
- *           "media_id": 1,
- *           "comment_text": "This is a comment",
- *           "created_at": "2024-01-26T09:38:08.000Z"
- *         }
- *       ]
- *     }
+ *     [
+ *       {
+ *         "comment_id": 1,
+ *         "user_id": 1,
+ *         "media_id": 1,
+ *         "comment_text": "This is a comment",
+ *         "created_at": "2024-01-26T09:38:08.000Z"
+ *       }
+ *     ]
  *
  * @apiError CommentsNotFound No comments found
  */
@@ -81,12 +77,14 @@
  * @apiName GetCommentsByUserToken
  * @apiGroup Comment
  *
- * @apiHeader {String} Authorization Bearer token for authentication.
+ * @apiHeader {String} Auth orization Bearer token for authentication.
  *
- * @apiSuccess {Object[]} comments List of comments for the user.
- * @apiSuccess {Number} comments.comment_id ID of the comment.
- * @apiSuccess {Number} comments.user_id ID of the user who commented.
- * @apiSuccess {String} comments.content Content of the comment.
+ * @apiSuccess {Object[]} Array of comments for the user
+ * @apiSuccess {Number} comment_id ID of the comment
+ * @apiSuccess {Number} user_id ID of the user who commented
+ * @apiSuccess {Number} media_id ID of the media
+ * @apiSuccess {String} comment_text Content of the comment
+ * @apiSuccess {String} created_at Timestamp when created
  *
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
@@ -94,9 +92,10 @@
  *       {
  *         "comment_id": 1,
  *         "user_id": 1,
- *         "content": "This is a comment"
- *       },
- *       ...
+ *         "media_id": 1,
+ *         "comment_text": "This is a comment",
+ *         "created_at": "2024-01-26T09:38:08.000Z"
+ *       }
  *     ]
  *
  * @apiError CommentsNotFound The comments were not found.
@@ -104,7 +103,7 @@
  * @apiErrorExample {json} Error-Response:
  *     HTTP/1.1 404 Not Found
  *     {
- *       "message": "No comments found"
+ *       "message": "No comments found for this user"
  *     }
  */
 /**
@@ -137,25 +136,20 @@
  *
  * @apiParam {Number} id Comment's unique ID.
  *
- * @apiSuccess {String} message Success message
- * @apiSuccess {Object} comment Comment object
- * @apiSuccess {Number} comment.comment_id ID of the comment
- * @apiSuccess {Number} comment.user_id ID of the user
- * @apiSuccess {Number} comment.media_id ID of the media
- * @apiSuccess {String} comment.comment_text Content of the comment
- * @apiSuccess {String} comment.created_at Timestamp when created
+ * @apiSuccess {Number} comment_id ID of the comment
+ * @apiSuccess {Number} user_id ID of the user
+ * @apiSuccess {Number} media_id ID of the media
+ * @apiSuccess {String} comment_text Content of the comment
+ * @apiSuccess {String} created_at Timestamp when created
  *
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
  *     {
- *       "message": "Comment found",
- *       "comment": {
- *         "comment_id": 1,
- *         "user_id": 1,
- *         "media_id": 1,
- *         "comment_text": "This is a comment",
- *         "created_at": "2024-01-26T09:38:08.000Z"
- *       }
+ *       "comment_id": 1,
+ *       "user_id": 1,
+ *       "media_id": 1,
+ *       "comment_text": "This is a comment",
+ *       "created_at": "2024-01-26T09:38:08.000Z"
  *     }
  *
  * @apiError CommentNotFound Comment not found
@@ -167,42 +161,30 @@
  *     }
  */
 /**
- * @api {put} /comments Update Comment
+ * @api {put} /comments/:id Update Comment
  * @apiName UpdateComment
  * @apiGroup Comment
  *
  * @apiHeader {String} Authorization Bearer token for authentication.
  *
+ * @apiParam {Number} id Comment's unique ID.
  * @apiParam {String} comment_text Text of the comment.
  *
- * @apiExample {json} Request-Example:
- *     PUT /comments
- *     {
- *       "comment_text": "This is an updated comment"
- *     }
- *
- * @apiSuccess {Number} comment_id ID of the updated comment.
- * @apiSuccess {Number} user_id ID of the user who commented.
- * @apiSuccess {Number} media_id ID of the media that was commented on.
- * @apiSuccess {String} content Updated content of the comment.
- * @apiSuccess {created_at} created_at Timestamp of when the comment was created.
+ * @apiSuccess {String} message Success message
  *
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
  *     {
- *       "comment_id": 1,
- *       "user_id": 1,
- *       "media_id": 1,
- *       "content": "This is an updated comment"
- *       "created_at": "2022-01-01T00:00:00.000Z"
+ *       "message": "Comment updated"
  *     }
  *
  * @apiError CommentNotFound The comment was not found.
+ * @apiError Unauthorized Not authorized to update this comment.
  *
  * @apiErrorExample {json} Error-Response:
  *     HTTP/1.1 404 Not Found
  *     {
- *       "message": "No comment found with this ID"
+ *       "message": "Comment not found"
  *     }
  */
 /**
